e12c4ae50bfc99aa657c5adc26c84241f5fb79a3
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-25 23:03:55 -0500

    Uploading

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, bringToSL }) {
+
+  const [filter, setFilter] = useState('')
+  const [category, setCategory] = useState('All')
+
+
+
+
+  function handleInput (e) {
+    setFilter(e.target.value)
+  }
+
+  bringToSL(filter)
+
+
+
+
+
+  function handleCategory (e) {
+    setCategory(e.target.value)
+  }
+
+  onCategoryChange(category)
+
+
+
+
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input type="text" name="search" placeholder="Search..." onChange={handleInput} />
+      <select name="filter" onChange={handleCategory}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,24 +5,66 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [bringToShoppingList, setBringToShoppingList] = useState('')
+  const [inputTextToSL, setInputTextToSL] = useState('')
+  const [inputCatToSL, setInputCatToSL] = useState('')
+  const [formData, setFormData] = useState(items)
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  function onCategoryChange (category) {
+    setSelectedCategory(category)
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  console.log(selectedCategory)
 
+  function bringToSL(filter) {
+    setBringToShoppingList(filter)
+  }
+
+
+
+
+  function bringInputTextUp(inputText) {
+    setInputTextToSL(inputText)
+  }
+
+  function bringInputCatUP(inputCategory) {
+    setInputCatToSL(inputCategory)
+  }
+
+  // console.log(inputTextToSL)
+  // console.log(inputCatToSL)
+
+
+  function bringStuffUp(e) {
+    e.preventDefault()
+    const addObject = { 
+      id: items.length + 1,
+      name: inputTextToSL, 
+      category: inputCatToSL 
+    }
+    setFormData((items) => [...items, addObject])
+    setInputTextToSL('')
+    setInputCatToSL('')
+  }
+
+  console.log(formData)
+
+  const itemsToDisplay = formData.filter((item) => {
+    if (selectedCategory === "All") return true;
     return item.category === selectedCategory;
   });
 
+
+
+  const filteredItems = itemsToDisplay.filter(item => item.name.includes(bringToShoppingList)) 
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm bringInputTextUp={bringInputTextUp} bringInputCatUp={bringInputCatUP} bringStuffUp={bringStuffUp} />
+      <Filter onCategoryChange={onCategoryChange} bringToSL={bringToSL}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredItems.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />  
         ))}
       </ul>
     </div>
